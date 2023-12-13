e34341798a2b1cbb926fd6c8b06e79ff6f29ab6d
Dina-S-A <87444987+Dina-S-A@users.noreply.github.com>
2023-12-13 14:38:27 +0000

    replit changes to github

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 const cookieParser = require('cookie-parser');
 
-const authRoutes = require ('./routes/authRoutes');
+const authRoutes = require('./routes/authRoutes');
 const coursesRoutes = require('./routes/coursesRoutes');
 const studentsRoutes = require('./routes/studentsRoutes');
 const cartRoutes = require('./routes/cartRoutes');
@@ -21,11 +21,11 @@ const app = express();
 //const dbURI = 'mongodb+srv://dragons:Passw0rd@mongodb.ho5iekl.mongodb.net/mongodb?retryWrites=true&w=majority';
 const dbURI = 'mongodb+srv://dragon:dragon1@cluster0.wozqhwt.mongodb.net/SushiWS?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-   .then((result) => app.listen(4000))//listening for request and starting the server
-   .catch((err) => console.log(err));
+  .then((result) => app.listen(4000))//listening for request and starting the server
+  .catch((err) => console.log(err));
 
 //register view engine
-app.set ('view engine', 'ejs'); //set lets us set some settings it looks into views folder for ejs files
+app.set('view engine', 'ejs'); //set lets us set some settings it looks into views folder for ejs files
 
 //Middleware and static files
 
@@ -33,9 +33,9 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use((req, res, next) => {
-    res.locals.path = req.path;
-    next();
-  });
+  res.locals.path = req.path;
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 
@@ -54,27 +54,27 @@ app.post('*', checkStudent);// call check user for every get request
 
 
 //Home page
-app.get ('/' , (req, res) => {
-    res.render('index' , { title : 'Home'}); 
-});        
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Home' });
+});
 
 
 
 
 //all courses routes
-app.use('/courses',coursesRoutes);
+app.use('/courses', coursesRoutes);
 
 //all students routes
-app.use('/students',studentsRoutes);
+app.use('/students', studentsRoutes);
 
 //all cart routes
-app.use('/cart',cartRoutes);
+app.use('/cart', cartRoutes);
 
 
 app.use(authRoutes); // that should be here after the checkTeacher is called 
 
 
- // 404 page
- app.use((req, res) => {    
-   res.status(404).render('404' , { title : '404'}); 
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
 });
